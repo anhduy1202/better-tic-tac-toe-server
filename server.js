76285f2a260088c47e7b1af98d9c2686b054ed13
Daniel Truong <aduy1122@gmail.com>
2021-08-27 09:35:43 -0700

    update cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const {getUserList, get_Current_User, user_Disconnect, join_User, playByTurn } =
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = require("socket.io")(httpServer, {
+const io = socketio(server, {
      cors: {
      origin: "http://localhost:8080",
      methods: ["GET","POST"]
